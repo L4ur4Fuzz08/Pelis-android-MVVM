bd094e4dc3dd06cd3e1abb4177357e9530753906
L4ur4Fuzz08 <la18ukadora@gmail.com>
2026-03-20 17:33:13 -0500

    defined remote service methods for movies

---
+++ app/src/main/java/com/example/pelisproapp/data/remote/PeliculaService.kt
@@ -0,0 +1,19 @@
+package com.example.pelisproapp.data.remote
+
+import com.example.pelisproapp.data.remote.model.PeliculaList
+
+class PeliculaService {
+
+    fun getUpcomingMovies(): PeliculaList{
+        return PeliculaList()
+    }
+
+    fun getTopRatedMovies(): PeliculaList{
+        return PeliculaList()
+    }
+
+    fun getPopularMovies(): PeliculaList{
+
+        return PeliculaList()
+    }
+}
