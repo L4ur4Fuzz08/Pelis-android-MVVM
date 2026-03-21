56912dc856c6ec76dcdfa14d1c833b7b20d9491f
L4ur4Fuzz08 <la18ukadora@gmail.com>
2026-03-20 21:17:48 -0500

    including the client in the webservice

--- app/src/main/java/com/example/pelisproapp/data/remote/interceptor/ApiKeyInterceptor.kt
+++ app/src/main/java/com/example/pelisproapp/data/remote/interceptor/ApiKeyInterceptor.kt
@@ -1,7 +1,13 @@
 package com.example.pelisproapp.data.remote.interceptor
 
+import com.example.pelisproapp.BuildConfig
+import com.example.pelisproapp.repository.WebSerice
+import com.google.gson.GsonBuilder
 import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import okhttp3.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiKeyInterceptor(private val apiKey: String) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
@@ -15,4 +21,20 @@ class ApiKeyInterceptor(private val apiKey: String) : Interceptor {
         val requestBuilder = originalRequest.newBuilder().url(url)
         return chain.proceed(requestBuilder.build())
     }
+}
+
+object RetrofitClient{
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(ApiKeyInterceptor(BuildConfig.API_KEY))
+        .build()
+    val webService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .client(okHttpClient)
+            .build()
+            .create(WebSerice::class.java)
+    }
+
 }
