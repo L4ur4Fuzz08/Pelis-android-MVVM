b59774d06287dae3ad50d0658640daacc3dfb3fa
L4ur4Fuzz08 <la18ukadora@gmail.com>
2026-03-20 21:44:05 -0500

    tooling the datasource data

---
+++ app/src/main/java/com/example/pelisproapp/data/remote/PeliculaDataSource.kt
@@ -0,0 +1,20 @@
+package com.example.pelisproapp.data.remote
+
+import com.example.pelisproapp.data.remote.model.PeliculaList
+import com.example.pelisproapp.repository.WebSerice
+
+class PeliculaDataSource(private val webSerice: WebSerice) {
+
+    suspend fun getUpcomingMovies(): PeliculaList{
+        return webSerice.getUpcomingMovies()
+    }
+
+    suspend fun getTopRatedMovies(): PeliculaList{
+        return webSerice.getTopRatedMovies()
+    }
+
+    suspend fun getPopularMovies(): PeliculaList{
+
+        return webSerice.getTopRatedMovies()
+    }
+}
