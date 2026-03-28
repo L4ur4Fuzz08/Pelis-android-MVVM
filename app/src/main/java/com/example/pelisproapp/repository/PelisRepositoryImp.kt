b22b0f5ee97dc13e54f36987b93da54eee882d71
L4ur4Fuzz08 <la18ukadora@gmail.com>
2026-03-28 09:14:36 -0500

    data source methods surrounded in a secondary thread

--- app/src/main/java/com/example/pelisproapp/repository/PelisRepositoryImp.kt
+++ app/src/main/java/com/example/pelisproapp/repository/PelisRepositoryImp.kt
@@ -2,18 +2,20 @@ package com.example.pelisproapp.repository
 
 import com.example.pelisproapp.data.remote.PeliculaDataSource
 import com.example.pelisproapp.data.remote.model.PeliculaList
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class PelisRepositoryImp(private val dataSource:PeliculaDataSource): PelisRepository {
-    override suspend fun getUpcomingMovies(): PeliculaList {
-        return dataSource.getUpcomingMovies()
+    override suspend fun getUpcomingMovies(): PeliculaList = withContext(Dispatchers.IO) {
+     dataSource.getUpcomingMovies()
     }
 
-    override suspend fun getTopRatedMovies(): PeliculaList {
-        return dataSource.getTopRatedMovies()
+    override suspend fun getTopRatedMovies(): PeliculaList = withContext(Dispatchers.IO)  {
+        dataSource.getTopRatedMovies()
     }
 
-    override suspend fun getPopularMovies(): PeliculaList {
-        return dataSource.getPopularMovies()
+    override suspend fun getPopularMovies(): PeliculaList = withContext(Dispatchers.IO)  {
+         dataSource.getPopularMovies()
     }
 
 
