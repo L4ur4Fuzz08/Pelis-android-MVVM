5523fe45bda5df12846ab22327212785ecf47aed
L4ur4Fuzz08 <la18ukadora@gmail.com>
2026-03-28 10:01:24 -0500

    main fragment created and added in navigation graph

---
+++ app/src/main/java/com/example/pelisproapp/presentacion/ui/fragment/PelisFragment.kt
@@ -0,0 +1,37 @@
+package com.example.pelisproapp.presentacion.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.pelisproapp.R
+import com.example.pelisproapp.databinding.FragmentPelisBinding
+
+
+class PelisFragment : Fragment() {
+
+
+    private  var _binding: FragmentPelisBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+       _binding = FragmentPelisBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+}
