4a7e80daf5c575f6ca0fbc19acfee286c06aaea0
L4ur4Fuzz08 <la18ukadora@gmail.com>
2026-03-28 17:53:38 -0500

    created baseviewholder for adapters

--- app/src/main/java/com/example/pelisproapp/repository/PelisRepository.kt
+++ app/src/main/java/com/example/pelisproapp/repository/PelisRepository.kt
@@ -1,9 +1,10 @@
 package com.example.pelisproapp.repository
 
 import com.example.pelisproapp.data.remote.model.PeliculaList
+import com.example.pelisproapp.presentacion.model.Pelicula
 
 interface PelisRepository {
-   suspend fun getUpcomingMovies(): PeliculaList
-   suspend fun getTopRatedMovies(): PeliculaList
-   suspend fun getPopularMovies(): PeliculaList
+   suspend fun getUpcomingMovies(): List<Pelicula>
+   suspend fun getTopRatedMovies(): List<Pelicula>
+   suspend fun getPopularMovies(): List<Pelicula>
 }
--- app/src/main/java/com/example/pelisproapp/repository/PelisRepositoryImp.kt
+++ app/src/main/java/com/example/pelisproapp/repository/PelisRepositoryImp.kt
@@ -2,20 +2,27 @@ package com.example.pelisproapp.repository
 
 import com.example.pelisproapp.data.remote.PeliculaDataSource
 import com.example.pelisproapp.data.remote.model.PeliculaList
+import com.example.pelisproapp.presentacion.model.Pelicula
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class PelisRepositoryImp(private val dataSource:PeliculaDataSource): PelisRepository {
-    override suspend fun getUpcomingMovies(): PeliculaList = withContext(Dispatchers.IO) {
-     dataSource.getUpcomingMovies()
+    override suspend fun getUpcomingMovies(): List<Pelicula> = withContext(Dispatchers.IO) {
+     dataSource.getUpcomingMovies().results.map {
+         it.toPelicula()
+     }
     }
 
-    override suspend fun getTopRatedMovies(): PeliculaList = withContext(Dispatchers.IO)  {
-        dataSource.getTopRatedMovies()
+    override suspend fun getTopRatedMovies(): List<Pelicula> = withContext(Dispatchers.IO)  {
+        dataSource.getTopRatedMovies().results.map {
+            it.toPelicula()
+        }
     }
 
-    override suspend fun getPopularMovies(): PeliculaList = withContext(Dispatchers.IO)  {
-         dataSource.getPopularMovies()
+    override suspend fun getPopularMovies(): List<Pelicula> = withContext(Dispatchers.IO)  {
+         dataSource.getPopularMovies().results.map {
+             it.toPelicula()
+         }
     }
 
 
