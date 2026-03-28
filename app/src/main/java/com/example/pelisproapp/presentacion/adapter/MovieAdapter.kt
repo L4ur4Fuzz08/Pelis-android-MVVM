d8e56548caf7b033695321c75a6da84b0204902a
L4ur4Fuzz08 <la18ukadora@gmail.com>
2026-03-28 17:54:56 -0500

    adapter for movie list implementation

---
+++ app/src/main/java/com/example/pelisproapp/presentacion/adapter/MovieAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.pelisproapp.presentacion.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pelisproapp.core.BaseViewHolder
+import com.example.pelisproapp.databinding.MovieItemBinding
+import com.example.pelisproapp.presentacion.model.Pelicula
+
+class MovieAdapter(private var movieList: List<Pelicula> = listOf(), private val itemClickListener: (Pelicula)->Unit): RecyclerView.Adapter<BaseViewHolder<*>>() {
+
+    interface OnMovieClickListener{
+        fun onMovieClick(movie: Pelicula)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
+        val itemBinding = MovieItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val holder = MoviesViewHolder(itemBinding)
+
+        itemBinding.root.setOnClickListener{
+            val position = holder.bindingAdapterPosition.takeIf{ it != DiffUtil.DiffResult.NO_POSITION}
+                ?:return@setOnClickListener
+            itemClickListener.invoke(
+                movieList[position]
+            )
+
+
+        }
+
+        return holder
+    }
+
+    fun updateList(newList: List<Pelicula>){
+        movieList = newList
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int = movieList.size
+
+    override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
+        when(holder){
+            is MoviesViewHolder-> holder.bind(movieList[position])
+        }
+    }
+
+    private inner class MoviesViewHolder(val binding: MovieItemBinding) : BaseViewHolder<Pelicula>(binding.root) {
+        override fun bind(item: Pelicula) {
+
+
+        }
+
+    }
+}
