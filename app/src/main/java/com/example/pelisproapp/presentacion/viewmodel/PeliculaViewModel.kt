90f840c9a6e6ab1104646df63fec93d418f4af7d
L4ur4Fuzz08 <la18ukadora@gmail.com>
2026-03-28 09:12:21 -0500

    configurantion changed to flow implementation

--- app/src/main/java/com/example/pelisproapp/presentacion/viewmodel/PeliculaViewModel.kt
+++ app/src/main/java/com/example/pelisproapp/presentacion/viewmodel/PeliculaViewModel.kt
@@ -1,21 +1,44 @@
 package com.example.pelisproapp.presentacion.viewmodel
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
 import com.example.pelisproapp.core.Resource
+import com.example.pelisproapp.data.remote.model.PeliculaList
+import com.example.pelisproapp.presentacion.PelisCollection
 import com.example.pelisproapp.repository.PelisRepository
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
-class PeliculaViewModel(private val repo: PelisRepository): ViewModel() {
+class PeliculaViewModel(private val repo: PelisRepository) : ViewModel() {
+
+    private val _movieState = MutableStateFlow<Resource<PelisCollection<PeliculaList, PeliculaList, PeliculaList>>>(Resource.Loading)
+    val movieState: StateFlow<Resource<PelisCollection<PeliculaList, PeliculaList, PeliculaList>>> = _movieState
 
 
+    init {
 
-    fun fetchUpcomingMovies() = liveData(Dispatchers.IO){
-        emit(Resource.Loading)
-        try {
-            emit(Resource.Success(repo.getUpcomingMovies()))
-        }catch (e: Exception){
-            emit(Resource.Failure(e))
+        fetchUpcomingMovies()
+    }
+
+
+    fun fetchUpcomingMovies() {
+        /*se abre una corrutina en el scope viewmodel para las llamadas del datasource*/
+        viewModelScope.launch {
+            _movieState.value = Resource.Loading
+            try {
+                //se llaman los 3 metodos del repository
+
+                val data = PelisCollection(
+                    repo.getUpcomingMovies(),
+                    repo.getTopRatedMovies(),
+                    repo.getPopularMovies()
+                )
+                _movieState.value = Resource.Success(data)
+            } catch (e: Exception) {
+                _movieState.value = Resource.Failure(e)
+            }
         }
+
     }
 }
