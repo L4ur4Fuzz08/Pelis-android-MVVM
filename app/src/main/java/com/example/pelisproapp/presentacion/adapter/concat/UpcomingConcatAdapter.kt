956772f39e7f1bed8e0ea84ce26fbacc0ea89375
L4ur4Fuzz08 <la18ukadora@gmail.com>
2026-03-29 10:11:36 -0500

    concat adapter for popular, toprated and upcoming categories

---
+++ app/src/main/java/com/example/pelisproapp/presentacion/adapter/concat/UpcomingConcatAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.pelisproapp.presentacion.adapter.concat
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pelisproapp.core.BaseConcatHolder
+import com.example.pelisproapp.databinding.UpcomingMovieRowBinding
+import com.example.pelisproapp.presentacion.adapter.MovieAdapter
+import com.example.pelisproapp.presentacion.model.Pelicula
+
+class UpcomingConcatAdapter(private val moviesAdapter: MovieAdapter): RecyclerView.Adapter<BaseConcatHolder<*>>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): BaseConcatHolder<*> {
+        val itemBinding = UpcomingMovieRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ConcatViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(
+        holder: BaseConcatHolder<*>,
+        position: Int
+    ) {
+        when(holder){
+            is ConcatViewHolder-> holder.bind(moviesAdapter)
+        }
+    }
+
+    fun updateAdapter(movies:List<Pelicula>){
+        moviesAdapter.updateList(movies)
+        notifyDataSetChanged()
+    }
+    //    Se define el tamaño de datos (1 solo recycler)
+    override fun getItemCount(): Int =1
+
+    private inner class ConcatViewHolder(val binding: UpcomingMovieRowBinding): BaseConcatHolder<MovieAdapter>(binding.root){
+        override fun bind(adapter: MovieAdapter) {
+            binding.rvUpcoming.adapter = adapter
+        }
+
+    }
+}
