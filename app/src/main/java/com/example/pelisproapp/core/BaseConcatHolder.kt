a0a039f9724639c438358904e46dc614611edfa2
L4ur4Fuzz08 <la18ukadora@gmail.com>
2026-03-29 14:57:20 -0500

    concat base holder and movie item

---
+++ app/src/main/java/com/example/pelisproapp/core/BaseConcatHolder.kt
@@ -0,0 +1,9 @@
+package com.example.pelisproapp.core
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseConcatHolder<T>(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    abstract fun bind(adapter: T)
+}
